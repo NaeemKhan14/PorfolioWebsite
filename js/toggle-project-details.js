1f677851097f3a65917c7a06a0db091348417160
Naeem Khan <naeemukhan14@gmail.com>
2021-06-15 22:20:09 +0400

    Moved JS to a separate file

---
+++ js/toggle-project-details.js
@@ -0,0 +1,7 @@
+function openNav(div_id) {
+    document.getElementById(div_id).style.width = "100%";
+}
+
+function closeNav(clicked_id) {
+    document.getElementById(clicked_id).style.width = "0%";
+}
